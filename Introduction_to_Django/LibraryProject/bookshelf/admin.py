3d537ad596c5078d301989dc71d312d49153466e
my-repo90 <malakdev024@gmail.com>
2025-02-21 23:07:47 +0100

    11th commit

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -3,11 +3,6 @@ from .models import Book
 # Register your models here.
 
 
-from django.contrib import admin
-from .models import Book
-
-# Register your models here.
-
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
