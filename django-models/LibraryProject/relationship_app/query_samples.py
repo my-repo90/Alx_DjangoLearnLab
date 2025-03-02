5b247b3798447596920b63a107e4591b2cb42f9e
my-repo90 <malakdev024@gmail.com>
2025-03-02 22:06:38 +0000

    29th commit

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,3 +4,4 @@ author = Author.objects.get(name=Author)
 library = Library.objects.get(name=Library)
 librarian = Librarian.objects.get(library=Librarian)
 "Library.objects.get(name=library_name)"
+"books.all()"
