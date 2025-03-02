e5f81df9b034f720b0ea3a0d9b010601757e595f
my-repo90 <malakdev024@gmail.com>
2025-03-02 22:48:10 +0000

    38th commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -106,19 +106,30 @@ def delete_book(request, book_id):
 #UserCreationForm()
 class CustomLogoutView(LogoutView):
     template_name = 'relationship_app/logout.html'
-	
+from .models import UserProfile
 
-@user_passes_test(User.userprofile.role == 'Admin')
+def has_role(user, role):
+    return UserProfile.objects.filter(user=user.id, role=role).exists()
+def is_admin(user):
+    return has_role(user, "Admin")
+def is_librarian(user):
+    return has_role(user, "Librarian")
+def is_member(user):
+    return has_role(user, "Member")
+
+# Views for Admin users
+@user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
-
-@user_passes_test(User.userprofile.role == 'Librarian')
+# View for Librarian users
+@user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
-
-@user_passes_test(User.userprofile.role == 'Member')
+# View for Member users
+@user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
 
