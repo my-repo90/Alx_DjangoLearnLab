407c7bc72107c6aad3eac36e4b50df92569d620a
my-repo90 <malakdev024@gmail.com>
2025-03-09 20:03:35 +0000

    commit

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-from django.contrib.auth.models import AbstractUser ,BaseUserManager
+from django.contrib.auth.models import AbstractUser ,BaseUserManager ,User
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField()
@@ -23,3 +23,20 @@ class CustomUserManager(BaseUserManager):
         
     def create_super_user():
         pass
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view article"),
+            ("can_create", "Can create article"),
+            ("can_edit", "Can edit article"),
+            ("can_delete", "Can delete article"),
+        ]
+
+    def __str__(self):
+        return self.title
