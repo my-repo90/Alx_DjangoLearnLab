654347a042bb3943f5e812e9bd548d1e07997a5c
my-repo90 <malakdev024@gmail.com>
2025-03-09 19:36:13 +0000

    commit

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
-
 from django.contrib.auth.models import AbstractBaseUser ,BaseUserManager
 
 class CustomUser(AbstractBaseUser):
