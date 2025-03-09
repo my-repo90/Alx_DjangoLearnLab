559f3dd6791fdfb343d7405795b44c8b8e41f129
my-repo90 <malakdev024@gmail.com>
2025-03-09 20:31:09 +0000

    commit

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -138,3 +138,20 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+SECURE_SSL_REDIRECT = True 
+SECURE_HSTS_SECONDS = 31536000  
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True  
+SECURE_HSTS_PRELOAD = True  
+
+SESSION_COOKIE_SECURE = True  
+CSRF_COOKIE_SECURE = True  
+
+
+X_FRAME_OPTIONS = 'DENY'  
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True 
+"SECURE_PROXY_SSL_HEADER",
+"HTTP_X_FORWARDED_PROTO" 
+"bookshelf.CustomUser"
