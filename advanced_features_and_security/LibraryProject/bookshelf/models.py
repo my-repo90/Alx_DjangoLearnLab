38e36b95340f15bfd009faa573d838d177decb05
my-repo90 <malakdev024@gmail.com>
2025-03-09 20:04:28 +0000

    commit

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -25,7 +25,7 @@ class CustomUserManager(BaseUserManager):
         pass
 
 
-class Article(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
