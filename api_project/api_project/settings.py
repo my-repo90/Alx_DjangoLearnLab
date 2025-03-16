edcd0ba224d66f6c24f547323088175a551c3ad9
my-repo90 <malakdev024@gmail.com>
2025-03-16 21:50:15 +0000

    4rd commit

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wn(7_m%0fmt1#sjz6h%6--ji-shn-j&g2u&$0vx&tw-0vmn99r'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
