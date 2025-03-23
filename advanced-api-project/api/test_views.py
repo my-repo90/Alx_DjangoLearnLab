8dcf84a60c8b0bdd6f5108bd794e9ae29acadee4
my-repo90 <malakdev024@gmail.com>
2025-03-23 04:06:55 +0000

    commit

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,4 +1,18 @@
-#from rest_framework.test import APIRequestFactory
-# ["self", "class", "APITestCase"]
-#["from rest_framework import status", "response.data"]
-#["self.client.login"]
+from rest_framework.test import APIRequestFactory
+from django.test import TestCase
+from rest_framework.test import APITestCase
+from .models import Book
+
+class BookAPITestCase(APITestCase):
+    def setUp(self):   
+        self.client.login(username="testuser", password="testpassword")
+   
+    def test_books_response(self):
+        response = self.client.get("/api/books/")
+        self.assertEqual(response.status_code, 200)
+
+
+
+    # ["self", "class", "APITestCase"]
+    #["from rest_framework import status", "response.data"]
+    #["self.client.login"]
